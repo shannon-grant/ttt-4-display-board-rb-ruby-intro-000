0076ed40016ba503843b22ec3a727393a99b2867
declarative-linter-0823 <grant.shannon90@gmail.com>
2020-04-01 20:45:45 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board(board)
+def display_board(board, board = [" "," "," "," "," "," "," "," "," "])
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
@@ -8,8 +8,6 @@ def display_board(board)
   puts "   |   |   "
   puts
 end
-
-board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(board)
   puts " #{board [0]}, #{board [1]}, #{board [2]}, #{board [3]}, #{board [4]}, #{board [5]}, #{board [6]}, #{board [7]}, #{board [8]}, #{board [9]}
