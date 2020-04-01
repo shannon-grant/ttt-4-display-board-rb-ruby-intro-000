92897d5f656321c5efd909e63847910ceee59c58
declarative-linter-0823 <grant.shannon90@gmail.com>
2020-04-01 21:18:40 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -15,4 +15,4 @@ def display_board(board)
   puts "#{board[0]}, #{board[1]}, #{board[2]}, #{board[3]}, #{board[4]}, #{board[5]}, #{board[6]}, #{board[7]}, #{board[8]}, #{board[9]} #{board [10]}, #{board[11]}"
 end
 
-print_display_board(board)
+display_board(board)
